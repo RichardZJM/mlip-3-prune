aad98773eb59978afb809c821ce67fd0fa509f57
RichardZJM <richardzjm@gmail.com>
2026-05-05 15:11:33 -0400

    Slight example update

--- test/examples/10.prune/test.sh
+++ test/examples/10.prune/test.sh
@@ -5,4 +5,17 @@ MLP_EXE=../../../bin/mlp
 
 # Performs pruning using config.json
 "$MLP_EXE" prune config.json 
+
+
+# MPI
+# export OPENBLAS_NUM_THREADS=1
+# export OMP_NUM_THREADS=1
+# export OMP_DYNAMIC=FALSE
+# export MKL_NUM_THREADS=1
+# export MKL_DYNAMIC=FALSE
+# export BLIS_NUM_THREADS=1
+# export VECLIB_MAXIMUM_THREADS=1
+# export GOTO_NUM_THREADS=1
+# export FLEXIBLAS_NUM_THREADS=1
+
 # mpirun -np 4 --map-by core --bind-to core "$MLP_EXE" prune config.json 
